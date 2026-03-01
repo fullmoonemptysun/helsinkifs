5b53c1238ff7efcf82495417650136469c36c6d5
Vinoy Kumar <v685958@gmail.com>
2026-02-28 20:48:23 -0700

    Finish exercise 2.7

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,58 +1,55 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {id:1, name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('');
+    const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
 
+    const handleInputChange = (event) => {
+        setNewName(event.target.value);
+    };
 
-  const handleInputChange = (event) => {
-    setNewName(event.target.value);
-  }
+    const handleFormSubmit = (event) => {
+        console.log(persons);
+        event.preventDefault();
 
-  const handleFormSubmit = (event) => {
-    console.log(persons);
-    event.preventDefault();
-    let newperson = {
-        id:persons.length + 1,
-        name: newName
-    }
-    setPersons([...persons,
-        
-        newperson]
-    )
+        if (
+            persons.find(
+                (person) => person.name.toLowerCase() === newName.toLowerCase(),
+            )
+        ) {
+            alert(`${newName} already exists!`);
+        } else {
+            let newperson = {
+                id: persons.length + 1,
+                name: newName,
+            };
+            setPersons([...persons, newperson]);
 
+            setNewName("");
+        }
+    };
 
-    setNewName('');
-  }
-
-
-
-  
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <ul>
-        {persons.map((person)=>
-            <li key={person.id}>{person.name}</li>
-        )}
-      </ul>
-      <form onSubmit={handleFormSubmit}>
+    return (
         <div>
-          name: <input onChange={handleInputChange} value={newName}/>
+            <h2>Phonebook</h2>
+            <ul>
+                {persons.map((person) => (
+                    <li key={person.id}>{person.name}</li>
+                ))}
+            </ul>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    name: <input onChange={handleInputChange} value={newName} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>debug: {newName}</div>
+            ...
         </div>
-        <div>
-          <button  type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+    );
+};
 
-      <div>debug: {newName}</div>
-      ...
-    </div>
-  )
-}
-
-export default App
+export default App;
