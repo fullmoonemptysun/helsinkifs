ec5f0b8c48f961a6f1fa5a52323b328c7acb77ac
Vinoy Kumar <v685958@gmail.com>
2026-04-14 23:21:17 -0700

    Finish Exertcise 3.1-3.3

---
+++ Part3/phonebookserver/index.js
@@ -0,0 +1,54 @@
+
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+
+const data = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (req, res)=>{
+    res.json(data).end();
+})
+
+app.get('/info', (req, res)=>{
+    res.send(`<p>Phone book has info for ${data.length} people </p> <p>${(new Date()).toLocaleString()}</p>`);
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
