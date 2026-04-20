906605f161bec688571b2dd8742e881976e608c7
Vinoy Kumar <v685958@gmail.com>
2026-04-20 01:25:15 -0700

    Apr 20th

--- Part3/phonebookserver/index.js
+++ Part3/phonebookserver/index.js
@@ -1,11 +1,13 @@
 
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 
-const data = [
+var data = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -48,11 +50,56 @@ app.get('/api/persons/:id', (req, res)=>{
 })
 
 
+app.delete('/api/persons/:id', (req,res)=>{
+    const contact = data.find((person)=>person.id === req.params.id);
+
+    console.log("Delete request received")
+
+    if(!contact){
+        res.statusMessage = "Contact does not exist on the server!"
+        return res.status(200).end()
+    }
+
+    data = data.filter(person => person.id !== contact.id);
+    res.status(200).send("Contact has been deleted");
+})
+
+
+app.post('/api/persons', (req, res) =>{
+    const id = Math.round(Math.random() * 500);
+
+    if(!req.body.number || !req.body.name){
+        console.log("ENTERED THIS")
+        return res.status(400).json({'error': 'missing number or contact name'});
+    }
+
+    
+    const duplicates = data.filter(person=>(String(person.number) === String(req.body.number)) && (person.name === req.body.name))
+    if(duplicates.length != 0){
+        return res.status(400).json({"error": "Contact already exists"})
+    }
+
+
+    const newPerson = {
+        'name' : req.body.name,
+        'number': req.body.number,
+        'id' : String(id)
+    }
+
+
+    data.push(newPerson)
+    res.status(200).json(newPerson)
+  
+})
 
 
 
+const unknownEndPoint = (req, res, next) => {
+    res.status(404).send({error: 'unknown endpoint'})
+    next()
+}
 
-
+app.use(unknownEndPoint)
 
 
 
